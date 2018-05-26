3bfb27994c16d4cbe1b431cb974ba1ed3b18f66d
xc <xc@DESKTOP-IT1P9OM>
2018-05-26 18:05:09 +0800

    提交

---
+++ gittest/src/test/java/gittest/test/git/GitTest.java
@@ -0,0 +1,27 @@
+/*
+ * 文  件  名：GitTest.java
+ * 描         述：
+ * 创  建  人：xc
+ * 创 建时间：2018年5月26日 下午6:04:50
+ */
+package gittest.test.git;
+
+
+/**
+ * TODO 补充注释
+ *
+ * @author xc
+ * @version V1.0 2018年5月26日
+ */
+public class GitTest {
+
+    /**
+     * TODO 补充注释
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
